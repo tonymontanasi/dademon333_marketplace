3c2988b5c285ee437e7f8eac974baad4c594891d
anatoly <safrigin@tochka.com>
2024-03-17 22:42:54 +0500

    fix: наследование модели от BaseModel

--- application/use_cases/sku/dto/set_sku_price.py
+++ application/use_cases/sku/dto/set_sku_price.py
@@ -1,8 +1,8 @@
 from uuid import UUID
 
-from pydantic import Field
+from pydantic import Field, BaseModel
 
 
-class SetSKUPriceInputDTO:
+class SetSKUPriceInputDTO(BaseModel):
     sku_id: UUID
     base_price: float = Field(..., ge=0.0)
